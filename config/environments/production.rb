fa14f5ece0e79a364a00c86a533a961a7137f17b
Estevan Vedovelli <evedovelli@gmail.com>
2015-08-29 00:14:40 -0300

    [issue/33] Updates env names for paperclip uploads to AWS S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Moka::Application.configure do
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
     }
   }
 
