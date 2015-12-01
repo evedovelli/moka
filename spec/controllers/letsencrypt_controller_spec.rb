e82aa6db07572d0ccec85f07cb20d3d35df34d68
Estevan Vedovelli <evedovelli@gmail.com>
2015-11-30 23:40:43 -0200

    [feature/40] Adds common instructions for let's encrypt challenge

--- spec/controllers/letsencrypt_controller_spec.rb
+++ spec/controllers/letsencrypt_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe LetsencryptController, :type => :controller do
 
-  describe "GET #challenge_0" do
+  describe "challenge" do
     it "responds to text requests" do
-      get :challenge_0, :format => 'text'
+      get :challenge, :format => 'text', :id => "test"
       expect(response.content_type).to eq(Mime::TEXT)
     end
     it "returns the required hash" do
-      get :challenge_0, :format => 'text'
-      expect(response.body).to eq('LZo2V6ot7n7FVSE1zxyOlntTalOlcCiXANi9AY71jVY.QGQUSxfEkcGfcMrZnOA8Ih7FQKuYkhiexBSTjJNWKSc')
+      get :challenge, :format => 'text', :id => "test"
+      expect(response.body).to eq('test.QGQUSxfEkcGfcMrZnOA8Ih7FQKuYkhiexBSTjJNWKSc')
     end
   end
 
