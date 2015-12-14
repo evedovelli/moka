a2f6465539fa6a6d1fc476e4f8c7ede8cfeeca8b
Estevan Vedovelli <evedovelli@gmail.com>
2015-12-14 21:28:16 -0200

    [feature/40] Enables https for AWS S3 images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,8 @@ Moka::Application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_protocol => :https
   }
 
   # Mailer for Devise Authentication
