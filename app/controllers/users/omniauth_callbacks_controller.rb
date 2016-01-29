02dfef79ac1ac92be671bc2c631e1d603b4f6981
Estevan Vedovelli <evedovelli@gmail.com>
2016-01-28 23:32:22 -0200

    [feature/6] Fixed some errors for Facebook auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    if request.env["omniauth.auth"].info.email.blank?
+      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
+      return
+    end
+
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
